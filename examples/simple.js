e83b6316816baba5d22504543bf4c1cd41d3ee11
Damon Oehlman <damon.oehlman@gmail.com>
2015-07-15 23:42:34 +1000

    Update the example to send a keydown and keyup

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,20 @@
 var simkey = require('..');
+var input = document.createElement('input');
 
-document.body.addEventListener('keydown', function(evt) {
-  console.log('received key code: ' + evt.keyCode);
+document.body.appendChild(input);
+
+input.addEventListener('keydown', function(evt) {
+  console.log('key down: ' + evt.keyCode);
+});
+
+input.addEventListener('keyup', function(evt) {
+  console.log('key up: ', evt.keyCode);
 });
 
 // simulate a keypress on the document body
-simkey(document.body, 65);
+simkey(input, 65);
 
 // simulate a number of keypress using simkey's partial application
-[65, 68].forEach(simkey(document.body));
+[65, 68]
+  .map(simkey(input))
+  .map(simkey(input, { type: 'keyup' }));
